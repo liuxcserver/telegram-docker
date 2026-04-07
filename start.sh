215318796ca2640eeccea589e4ac756748547ecb
liuxc <867251765@qq.com>
2026-04-08 04:12:42 +0800

    master: 修改使用linux版本的telegram

--- start.sh
+++ start.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 set -e
 
-# 创建 VNC 密码
 mkdir -p ~/.vnc
 echo -e "${VNC_PASSWD}\n${VNC_PASSWD}" | vncpasswd ~/.vnc/passwd
 chmod 600 ~/.vnc/passwd
 
-# 启动虚拟桌面
 Xvfb ${DISPLAY} -screen 0 1280x720x16 &
 sleep 1
 
-# 窗口管理器
 openbox &
 
-# VNC
 vncserver ${DISPLAY} -geometry 1280x720 -depth 16 &
 
-# noVNC
 ${NO_VNC_HOME}/utils/novnc_proxy --vnc localhost:${VNC_PORT} --listen ${NO_VNC_PORT} &
 
-# 启动 Telegram
 telegram
